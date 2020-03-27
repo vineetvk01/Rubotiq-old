1fe325d07f42805e780a569024db21c8632cca14
Vineet Srivastav <vineetvk01@gmail.com>
2020-03-28 04:30:58 +0530

    login-view-button

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { DContainer, DRow, DCol } from '../components/Container';
+import { DButton } from '../components/Button';
 
-const HomePage = () => {
+const Login = () => {
   return (
     <DContainer fluid>
       <DRow>
-        <DCol>Login</DCol>
+        <DCol md={{ span: 6, offset: 3 }}>
+          <h5 className="bold-heading text-center">Connect Now !</h5>
+          <hr />
+          <DButton variant="outline-dark" size="lg" block><img height="32" src="./images/google.svg" alt="Google Logo" /> | Google</DButton>
+          <DButton variant="outline-dark" size="lg" block><img height="32" src="./images/github.svg" alt="Google Logo" /> | Github</DButton>
+        </DCol>
+      </DRow>
+      <div style={{ height: '100px' }} />
+      <DRow>
+        <DCol xs={4}>
+          <div className="text-center">
+            <img height="80" src="./images/login/1.svg" alt="Person Running" />
+          </div>
+        </DCol>
+        <DCol xs={4}>
+          <div className="text-center">
+            <img height="80" src="./images/login/2.svg" alt="Group" />
+          </div>
+        </DCol>
+        <DCol xs={4}>
+          <div className="text-center">
+            <img height="80" src="./images/login/3.svg" alt="Chat" />
+          </div>
+        </DCol>
       </DRow>
     </DContainer>
   );
 };
 
-export default HomePage;
+export default Login;
